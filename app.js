d3aa59d134556e168da3edb5a61edd19c401b851
Alenochka2003 <alena.filina@icloud.com>
2024-10-10 16:36:58 +0200

    comm

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import path from 'path';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Новый пользователь подключился');
+
+
+    socket.on('chat message', (msg) => {
+        console.log('Сообщение: ' + msg);
+        
+        io.emit('chat message', msg);
+    });
+
+   
+    socket.on('disconnect', () => {
+        console.log('Пользователь отключился');
+    });
+});
+
+// Запуск сервера
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Сервер работает на порту ${PORT}`);
+});
